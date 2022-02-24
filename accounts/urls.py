ba20dc291ba02843fee336bfcc441692fc6ce904
shanemcg992 <x00175187@mytudublin.ie>
2022-02-24 12:46:25 +0000

    sign up and sign in and sign out added to top navbar

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import signupView
+from .views import signupView, signinView, signoutView
 
 urlpatterns = [
     path('create/', signupView, name='signup'),
+    path('login/', signinView, name='signin'),
+    path('logout/', signoutView, name='signout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import CustomUser
 from .forms import CustomUserCreationForm
 from django.contrib.auth.models import Group
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
 
 # Create your views here.
 def signupView(request):
@@ -16,3 +18,23 @@ def signupView(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'registration/signup.html', {'form':form})
+
+def signinView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('movies:allFilmGenre')
+            else:
+                return redirect('signup')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/signup.html', {'form':form})
+
+def signoutView(request):
+    logout(request)
+    return redirect('signin')
