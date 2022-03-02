2a6a27fae9dde0e00a2daae2c039c491042c5615
aaron doyle <x00172752@mytudublin.ie>
2022-03-02 23:14:15 +0000

    added unit tests

---
+++ unit tests/Search_Tests.py
@@ -0,0 +1,36 @@
+import imp
+import unittest
+
+from search_app.admin import admin
+
+class test_search_bar(unittest.TestCase):
+    search_bar_obj = admin()
+
+    def setUp(self):
+        self.sign_in_obj = admin()
+
+    def tearDown(self):
+        del self.search_bar_obj
+
+
+    def test_invalid_movie(self):
+
+        search = "simpsons"
+        output = "no results found"
+        
+        
+        result = self.search_bar_obj.search(search)
+
+
+        self.assertEquals(output, result)
+
+    
+    def test_valid_movie(self):
+
+        search = "hal" or "halloween"
+        output = "*film is displayed*"
+
+        result = self.search_bar_obj.search(search)
+
+        self.assertEquals(output, result)
+
---
+++ unit tests/user_tests.py
@@ -0,0 +1,51 @@
+import email
+from re import I
+from unicodedata import name
+import unittest
+
+from accounts.views import views
+
+class test_sign_in(unittest.TestCase):
+    sign_in_obj = views()
+
+    def setUp(self):
+        self.sign_in_obj = views()
+
+    def tearDown(self):
+        del self.sign_in_obj
+
+
+    def test_create_user(self):
+
+        details = "test1", "shanemcg992@gmail.com", "21", "laylabenji12", "laylabenji12"
+
+        output = self.sign_in_obj.create_user(details)
+
+        self.assertEquals(output, "User details:", details)
+
+
+    def test_invalid_credentials(self):
+
+        details = "test1", "shanemcg992@gmail.com", "21", "layla1", "layla1"
+
+        error = "Password invalid. Password must be more than 8 characters long."
+
+        self.assertEquals(details,"\n",error)
+
+    
+    def test_sign_in_valid_data(self):
+
+        details = "project2", "Aaronfinnshane1"
+
+        output = self.sign_in_obj.sign_in(details)
+
+        self.assertEquals(output, "Welcome User:", details)
+
+    def test_sign_in_invalid_data(self):
+
+        details = "projec", "Aaronfinnshane1"
+
+        error = "Please enter correct username or password"
+
+        self.assertEquals(error)
+
